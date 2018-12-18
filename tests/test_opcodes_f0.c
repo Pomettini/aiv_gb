8dfd397e21bea58f14e317c4e387fc634ae10352
Giorgio Pomettini <giorgio.pomettini@gmail.com>
2018-12-18 12:35:15 +0100

    Added placeholder opcodes f0

--- tests/test_opcodes_f0.c
+++ tests/test_opcodes_f0.c
@@ -1,7 +1,24 @@
 #include <aiv_unit_test.h>
 #include <aiv_gb.h>
 
+TEST(pop_af)
+{
+    aiv_gameboy gb;
+    aiv_gb_init(&gb);
+
+    gb.sp = 0xff;
+    aiv_gb_memory_write16(&gb, gb.sp - 1, 0xff00);
+    gb.sp -= 2;
+
+    gb.cartridge[0] = 0xf1;
+    aiv_gb_tick(&gb);
+
+    ASSERT_THAT(gb.af == 0xff00);
+    ASSERT_THAT(gb.ticks == 12);
+    ASSERT_THAT(gb.pc == 3);
+}
+
 void aiv_gb_tests_run_opcodes_f0()
 {
-    
+    RUN_TEST(pop_af);
 }
